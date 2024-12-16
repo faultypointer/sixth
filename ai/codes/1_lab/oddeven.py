a21f0cbab85845be938fe0c3ca6c7ca29f37c8aa
faultypointer <faultypointer@proton.me>
2024-12-16 20:29:57 +0545

    odd even

---
+++ ai/codes/1_lab/oddeven.py
@@ -0,0 +1,11 @@
+def odd_or_even():
+    num = int(input("Please enter a number: "))
+    if num % 2 == 0:
+        print(f"{num} is even.")
+    else:
+        print(f"{num} is odd.")
+
+
+if __name__ == "__main__":
+    odd_or_even()
+    
